6f3ad0d9b4902265fcc47aac0a31f889ae7577e0
ToSnow <tommrrara@foxmail.com>
2025-08-12 17:44:08 +0800

    del: 删除Vue版本的electron-app

--- electron-app.old/src/locales/index.ts
+++
@@ -1,6 +0,0 @@
-export { schema as enUS } from './en-US'
-export { type MessageSchema, schema as zhCN } from './zh-CN'
-
-export type LocaleKeys
-  = | 'en-US'
-    | 'zh-CN'
--- electron-app.old/src/preload/index.ts
+++
@@ -1,4 +0,0 @@
-import { electronAPI } from '@electron-toolkit/preload'
-import { contextBridge } from 'electron'
-
-contextBridge.exposeInMainWorld('electron', electronAPI)

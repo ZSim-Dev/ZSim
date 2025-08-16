1a17554d32ca47de426c4acb97bfc8dfccb3caf8
Steinwaysj <1012399286@qq.com>
2025-08-16 15:06:07 +0800

    feat:将复杂逻辑的Record类抽象出来

--- zsim/sim_progress/Buff/BuffXLogic/_buff_record_base_class.py
+++ zsim/sim_progress/Buff/BuffXLogic/_buff_record_base_class.py
@@ -2,14 +2,24 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from ...Character.character import Character
+    from ...Character.Alice import Alice
+    from ...Character.Yixuan import Yixuan
     from ...Enemy import Enemy
     from ..buff_class import Buff
+    from ...data_struct.ActionStack import ActionStack
+    from ...Preload.PreloadDataClass import PreloadData
 
 
 class BuffRecordBaseClass:
     def __init__(self):
-        self.char: "Character | None" = None
+        self.char: "Character | None | Alice | Yixuan" = None
         self.sub_exist_buff_dict: dict[str, "Buff"] | None = None
         self.dynamic_buff_list: dict[str, list] | None = None
         self.enemy: "Enemy | None" = None
-        self.trans_ratio: float = 1.6  # 转化比率
+        self.equipper: "str | None" = None
+        self.action_stack: "ActionStack | None" = None
+        self.event_list: list | None = None
+        self.preload_data: "PreloadData | None" = None
+        self.char_obj_list: "list[Character] | None" = None
+        self.na_skill_level: "int | None" = None
+        self.trans_ratio: float = 0

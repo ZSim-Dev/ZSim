083cc0aa379c3eda0d533fe6dcd8930aab581dad
Lo <l709937065@gmail.com>
2025-09-04 01:22:34 +0800

    chore: format

--- electron-app/src/main.tsx
+++ electron-app/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './i18n';
 import { LanguageProvider } from './contexts';
-import "@/styles/main.css";
+import '@/styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- electron-app/vite.config.ts
+++ electron-app/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // @ts-expect-error no @types
 import SemiPlugin from 'vite-plugin-semi-theme';
 import tailwindcss from '@tailwindcss/vite';
-import {URL,fileURLToPath} from "url"
+import { URL, fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({

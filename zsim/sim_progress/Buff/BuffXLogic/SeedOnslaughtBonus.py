f6d01eafdcf9d6ecdef5e17561b5848c9a60feef
TigerSkin <1012399286@qq.com>
2025-09-13 07:44:45 +0800

    feat: 席德强袭Buff实现

--- zsim/sim_progress/Buff/BuffXLogic/SeedOnslaughtBonus.py
+++ zsim/sim_progress/Buff/BuffXLogic/SeedOnslaughtBonus.py
@@ -8,10 +8,12 @@ class SeedOnslaughtBonusRecord(BRBC):
 
 
 class SeedOnslaughtBonus(Buff.BuffLogic):
+    """席德的强袭Buff复杂逻辑"""
     def __init__(self, buff_instance):
         super().__init__(buff_instance)
         self.buff_instance: Buff = buff_instance
         self.xjudge = self.special_judge_logic
+        self.xhit = self.special_exit_logic
         self.buff_0: "Buff | None" = None
         self.record: BRBC | None = None
 
@@ -31,8 +33,17 @@ class SeedOnslaughtBonus(Buff.BuffLogic):
         self.record = self.buff_0.history.record
 
     def special_judge_logic(self, **kwargs):
+        """席德的强袭状态早就已经记录在席德的特殊资源中了，所以这里不需要重复判断，只需要直接调用方法判断是否生效即可"""
         self.check_record_module()
         self.get_prepared(char_CID=1461)
         assert self.record is not None, (
             f"【Buff初始化警告】{self.buff_instance.ft.index}的复杂逻辑模块未正确初始化，请检查函数"
         )
+        from zsim.sim_progress.Character.Seed import Seed
+        assert isinstance(self.record.char, Seed), False
+        return self.record.char.onslaught_active
+
+    def special_exit_logic(self, **kwargs):
+        """强袭Buff的退出逻辑和生效逻辑相反，所以这里需要调用席德的方法退出强袭状态"""
+        return not self.xjudge
+
